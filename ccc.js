dfe1e18c04a3aef920cda820b993c11c7f91004e
Rubai Bian <lnbyk@udel.edu>
2021-01-13 11:21:11 -0500

    add doc for api

--- ccc.js
+++ ccc.js
@@ -39,6 +39,16 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 // PORT
 const port = process.env.PORT || 3000;
